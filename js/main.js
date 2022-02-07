a69702cae7cd3689651d75ec1f4362ad829b9769
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-07 20:43:25 +0100

    Esercizio completato e validato

--- js/main.js
+++ js/main.js
@@ -7,17 +7,16 @@ function numRandom (min, max){
 //funzione per creare un array di numeri casuali
 function createRandomNumber(array,cicli,min,max){
 
-    let i = 0;
-    while(i < cicli){
-        let numero = 0 ;
+    
+    for(let i = 0; i < cicli;i++){
 
+        let numero = 0 ;
+        //ciclo do while per non avere duplicati
         do{
             numero = numRandom(min,max);   
         }while(array.includes(numero))
 
-        array.push(numero)
-
-        i++
+        array.push(numero);  
     }
 }
 
@@ -29,7 +28,7 @@ function userNumberPrompt(array,numeri) {
         let numero ;
         do{
             numero = parseInt(prompt("Inserisci un numero"));
-        }while(isNaN(numero) || numero < 0 || numero >100)
+        }while(isNaN(numero) || numero < 0 || numero >100)//validazione del numero 
 
         array.push(numero);
     }
@@ -37,12 +36,14 @@ function userNumberPrompt(array,numeri) {
 
 //funzione per cambiare il tipo di display
 function changeDisplay(ele1,ele2){
+
     ele1.classList.add('none');
     ele2.classList.remove('none');
 }
 
 //creazione di un array con i numeri uguali di due array messi a confronto
 function numCompare(array1, array2,array3){
+
     array1.forEach(value =>{
         if(array2.includes(value)){
           array3.push(value);
@@ -71,16 +72,20 @@ function drawResult(container, array){
 let numberBoxHtml = document.getElementById('number-box');
 let rememberHtml = document.getElementById('remember');
 let afterRememberHtml = document.getElementById('after-remember');
-let containerHtml = document.getElementById('container')
+let containerHtml = document.getElementById('container');
+
 //inizializzo gli array vuoto
 const numberToRemember = [];
 const numUser = [];
 const numCorrect = [];
 //riempio l'array con 5 numeri casuali da 0 a 100
-createRandomNumber(numberToRemember,5,0,100)
+createRandomNumber(numberToRemember,5,0,100);
 //inserisco i numeri all'interno dell'html
 numberBoxHtml.innerHTML = numberToRemember;
-//il codice viene elaborato dopo 30 secondi
+
+/************************************************
+ prendo i numeri con il prompt e do l'esito
+************************************************/
 setTimeout (function (){
 
     //cambio il display ai div nell'html
@@ -95,4 +100,4 @@ setTimeout (function (){
         //inserisco il risultato nell'html
         drawResult(containerHtml, numCorrect);        
     }, 250);
-},1000*30)
+},1000*30);
